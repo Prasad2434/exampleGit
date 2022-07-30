b726cf0a836e11aebfc98d9596bd1c68df78220e
Prasad Gajananrao Kulkarni <QQA0429@QAPITOL.AD>
2022-07-30 13:55:02 +0530

    Add project

---
+++ src/main/java/Demo/GetApi.java
@@ -0,0 +1,20 @@
+package Demo;
+
+import org.testng.annotations.Test;
+
+public class GetApi 
+{
+	@Test
+	public void add ()
+	{
+		System.out.println("Add Operation");
+	}
+
+	
+	@Test
+	public void sub()
+	{
+		System.out.println("Sub Operation");
+	}
+	
+}
